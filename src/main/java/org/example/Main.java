0a04eb9da61582d41e541d366159e983852ce6ae
Pro100Den07 <drozhkov07@gmail.com>
2024-08-23 19:40:02 +0300

    Додана iнформацiя клiєнта

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         // Створення об'єкта Customer
-        Customer customer = new Customer("", "denys123@hmail.com", "+");
+        Customer customer = new Customer("Сергій ", "serg123@hmail.com", "+38012345678");
 
         // Відображення інформації про клієнта
         customer.displayInfo();
